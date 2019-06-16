d1a9cd68c29b7b72e75c5feac82dccceab400190
MohamadHijazii <46487299+mhamad98@users.noreply.github.com>
2019-06-16 20:06:12 +0300

    done

--- Organizator/Program.cs
+++ Organizator/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
-
+using System.IO;
+using System.Collections.Generic;
 namespace Organizator
 {
     class Program
     {
+        static List<string> direc;
+        static void organize(string path)
+        {
+            string[] ls = Directory.GetFiles(path);
+            direc = new List<string>();
+
+            foreach (string s in ls)
+            {
+                FileInfo f = new FileInfo(s);
+                string[] splited = f.LastWriteTime.ToString().Split("-");
+                string time = (splited[1] + "-20" + splited[2]).Split(" ")[0];
+                if (direc.Contains(time))
+                {
+                    string newPath = path + "/" + time + "/" + f.Name;
+                    File.Move(path + "/" + f.Name, newPath);
+                }
+                else
+                {
+                    Directory.CreateDirectory(path + "/" + time);
+                    string newPath = path + "/" + time + "/" + f.Name;
+                    File.Move(path + "/" + f.Name, newPath);
+                    direc.Add(time);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Enter the full path: ");
+            string path = Console.ReadLine();
+            organize(path);
+            Console.WriteLine("done!");
         }
     }
 }
